32717acf4b402a89da55595419bb742c30fb354c
Bissuelquentin <qbissuel@gmail.com>
2020-01-07 12:13:59 +0100

    Changes parser to parserTXT and modification in main

--- src/main.py
+++ src/main.py
@@ -1,5 +1,14 @@
-import matplotlib
+import parserTXT
 
-def main() :
-
-    return 1
+if __name__ == '__main__':
+    parserTXT.ficParsing("scen01")
+    parserTXT.ficParsing("scen02")
+    parserTXT.ficParsing("scen03")
+    parserTXT.ficParsing("scen04")
+    parserTXT.ficParsing("scen05")
+    parserTXT.ficParsing("scen06")
+    parserTXT.ficParsing("scen07")
+    parserTXT.ficParsing("scen08")
+    parserTXT.ficParsing("scen09")
+    parserTXT.ficParsing("scen10")
+    parserTXT.ficParsing("scen11")
---
+++ src/parserTXT.py
@@ -0,0 +1,23 @@
+import ficWriter
+
+def ficParsing(nomscen):
+    ctrtab = []
+    vartab = []
+    domtab = []
+
+    with open('../CELAR/'+ nomscen+ '/ctr.txt', "r") as f:
+        for line in f.readlines():
+            # Traiter la ligne et ainsi de suite ...
+            ctrtab.append(line.split())
+
+    with open('../CELAR/'+ nomscen +'/var.txt', "r") as f:
+        for line in f.readlines():
+            # Traiter la ligne et ainsi de suite ...
+            vartab.append(line.split())
+
+    with open('../CELAR/'+ nomscen +'/dom.txt', "r") as f:
+        for line in f.readlines():
+            # Traiter la ligne et ainsi de suite ...
+            domtab.append(line.split())
+
+    ficWriter.createFile(nomscen, vartab, ctrtab, domtab)
